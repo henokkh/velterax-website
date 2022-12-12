62bdfc9a12e9f836998cb1024c7b0f40e991cd5e
Wossen E <w.hailemariam11@gmail.com>
2022-12-12 11:24:25 +0100

    minor updates

--- components/hero.tsx
+++ components/hero.tsx
@@ -17,15 +17,16 @@ function Hero() {
           height="500"
         />
         <h1 className="mx-auto mb-10 mt-14 max-w-2xl text-3xl font-bold leading-normal md:text-4xl">
-          All your files in one secure location, assessible anywhere.
+        We Create a Magnificent Digital Project to Grow &nbsp; 
+        <span className="text-accentCyan">
+        Your Bussines.
+        </span>
         </h1>
         <p className="mx-auto mb-10 max-w-sm text-sm md:max-w-xl md:text-lg">
-          Fylo stores all your most important files in one secure location.
-          Access them wherever you need, share and collaborate with friends
-          family, and co-workers.
+        Tailor-made service from a single source - we create solutions for you and together with you for your digital projects. We tailor our services to the needs of freelancers , medical practices , craftsmen , service providers or retailers , regardless of whether you want to give your established SME or startup a digital foundation. Our service is also aimed at clubs , associations , societies and restaurants , event locations , communities , churches ,Municipalities , schools , adult education centers or  private individuals - we provide answers to your questions about IT, the Internet and marketing.
         </p>
         <button className="w-52 rounded-full bg-accentCyan p-3 hover:scale-95">
-          Get Started
+          Let's Talk
         </button>
       </div>
     </section>
