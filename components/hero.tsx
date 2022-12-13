267e749b542914b71d9abb913f141a748db46110
Henok Hailemariam <henokhm2@gmail.com>
2022-12-13 20:03:31 +0300

    Fix merge conflict

--- components/hero.tsx
+++ components/hero.tsx
@@ -17,20 +17,7 @@ function Hero() {
           width="500"
           height="500"
         />
-<<<<<<< Updated upstream
-        <h1 className="mx-auto mb-10 mt-14 max-w-2xl text-3xl font-bold leading-normal md:text-4xl">
-        We Create a Magnificent Digital Project to Grow &nbsp;
-        <span className="text-accentCyan">
-        Your Bussines.
-        </span>
-        </h1>
-        <p className="mx-auto mb-10 max-w-sm text-sm md:max-w-xl md:text-lg">
-        Tailor-made service from a single source - we create solutions for you and together with you for your digital projects. We tailor our services to the needs of freelancers , medical practices , craftsmen , service providers or retailers , regardless of whether you want to give your established SME or startup a digital foundation. Our service is also aimed at clubs , associations , societies and restaurants , event locations , communities , churches ,Municipalities , schools , adult education centers or  private individuals - we provide answers to your questions about IT, the Internet and marketing.
-        </p>
-        <button className="w-52 rounded-full bg-accentCyan p-3 hover:scale-95">
-          Let's Talk
-        </button>
-=======
+
         <h1 className="mx-auto mb-10 mt-14 max-w-2xl text-3xl font-bold  md:text-4xl">
           We Create a Magnificent Digital Project to Grow &nbsp;
           <span className="text-primaryAccent">Your Bussines.</span>
@@ -57,7 +44,6 @@ function Hero() {
             />
           </button>
         </a>
->>>>>>> Stashed changes
       </div>
     </section>
   );
